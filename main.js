626ec655efcb2a3c91153d08383230c509b467c0
Luis Gabriel <luis.gabo.rex@gmail.com>
2025-08-31 21:27:38 -0600

    feat: add active state toggle for action buttons and initEvents

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
 const $modeBtn = document.querySelector(".mode-btn");
 const $body = document.querySelector("body");
-const $logo = document.querySelector(".logo")
+const $logo = document.querySelector(".logo");
+const $showAllBtn = document.querySelector(".showAll");
+const $showActiveBtn = document.querySelector(".showActive");
+const $showInactiveBtn = document.querySelector(".showInactive");
+const $filtersBtn = document.querySelectorAll(".filters button");
 
-$modeBtn.addEventListener('click', (e) => {
+initEvents();
+
+function initEvents() {
+   $modeBtn.addEventListener('click', changeTheme);
+   $showAllBtn.addEventListener('click', showAll);
+   $showActiveBtn.addEventListener('click', showActive);
+   $showInactiveBtn.addEventListener('click', showInactive);
+   $filtersBtn.forEach(btn => {
+      btn.addEventListener('click', () => setActive(btn, $filtersBtn));
+   });
+
+}
+
+function changeTheme() {
    $body.classList.toggle("light");
-   $modeBtn.classList.toggle("light");  
-   $logo.classList.toggle("light"); 
+   $modeBtn.classList.toggle("light");
+   $logo.classList.toggle("light");
    if ($logo.classList.contains("light")) {
       $logo.src = "./assets/images/logo-light.svg";
    } else {
       $logo.src = "./assets/images/logo-dark.svg";
    }
-});
+}
+
+function showAll() {
+   console.log("showing all");
+   
+}
+
+function showActive() {
+   console.log("showing active");
+
+}
+
+function showInactive() {
+   console.log("showing inactive");
+
+}
+
+function setActive(btn, filtersBtn) {
+   if (!btn.classList.contains("active-btn")) {
+      filtersBtn.forEach(btn => btn.classList.remove("active-btn"));
+      btn.classList.add("active-btn")
+   }
+}
