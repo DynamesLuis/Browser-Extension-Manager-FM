5887eeb6bb10984ad6f524673cb7656547ec2aff
Luis Gabriel <luis.gabo.rex@gmail.com>
2025-08-31 18:45:10 -0600

    style: add light theme colors

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const $modeBtn = document.querySelector(".mode-btn");
 const $body = document.querySelector("body");
+const $logo = document.querySelector(".logo")
 
 $modeBtn.addEventListener('click', (e) => {
    $body.classList.toggle("light");
-   $modeBtn.classList.toggle("light");     
+   $modeBtn.classList.toggle("light");  
+   $logo.classList.toggle("light"); 
+   if ($logo.classList.contains("light")) {
+      $logo.src = "./assets/images/logo-light.svg";
+   } else {
+      $logo.src = "./assets/images/logo-dark.svg";
+   }
 });
